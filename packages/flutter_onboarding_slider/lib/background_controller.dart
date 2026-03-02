1a91d182501b70cad60cdf93c0501ab6f1c7eb37
Adams^ <110671354+adampermana@users.noreply.github.com>
2026-03-02 13:36:04 +0700

    Refactor background controller for page indicators circle

--- packages/flutter_onboarding_slider/lib/background_controller.dart
+++ packages/flutter_onboarding_slider/lib/background_controller.dart
@@ -19,22 +19,34 @@ class BackgroundController extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.only(bottom: 8),
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: _buildPageIndicator(context),
-      ),
-    );
+    return indicatorAbove
+        ? Container(
+            padding: EdgeInsets.only(bottom: 10),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: _buildPageIndicator(context),
+            ),
+          )
+        : (currentPage == totalPage - 1) && hasFloatingButton
+        ? SizedBox.shrink()
+        : Container(
+            padding: EdgeInsets.only(bottom: 10),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: _buildPageIndicator(context),
+            ),
+          );
   }
 
   /// List of the slides Indicators.
   List<Widget> _buildPageIndicator(BuildContext context) {
     List<Widget> list = [];
     for (int i = 0; i < totalPage; i++) {
-      list.add(i == currentPage
-          ? _indicator(true, context)
-          : _indicator(false, context));
+      list.add(
+        i == currentPage
+            ? _indicator(true, context)
+            : _indicator(false, context),
+      );
     }
     return list;
   }
@@ -42,14 +54,18 @@ class BackgroundController extends StatelessWidget {
   /// Slide Controller / Indicator.
   Widget _indicator(bool isActive, BuildContext context) {
     return AnimatedContainer(
-      duration: const Duration(milliseconds: 200),
-      margin: const EdgeInsets.symmetric(horizontal: 4.0),
+      duration: Duration(milliseconds: 150),
+      margin: EdgeInsets.only(
+        left: 8.0,
+        right: 8.0,
+        bottom: indicatorAbove ? indicatorPosition : 28,
+      ),
       height: 8.0,
-      width: isActive ? 24.0 : 8.0,
+      width: 8.0,
       decoration: BoxDecoration(
         color: isActive
-            ? controllerColor ?? const Color(0xFF6B2D8B)
-            : (controllerColor ?? const Color(0xFF6B2D8B)).withOpacity(0.3),
+            ? controllerColor ?? Colors.white
+            : (controllerColor ?? Colors.white).withOpacity(0.5),
         borderRadius: BorderRadius.all(Radius.circular(12)),
       ),
     );
